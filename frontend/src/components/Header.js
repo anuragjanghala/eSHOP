0edb9956a5633079a22a1ab94a994b8b1145b3a2
anuragjanghala <ajanghala@gmail.com>
2021-01-31 23:20:33 +0000

    added brand icon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavbarBrand } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
+                
                     <LinkContainer to='/'>
-                        <Navbar.Brand>eSHOP</Navbar.Brand>
+                        <Navbar.Brand>
+                            <i class="fas fa-shopping-bag"></i>
+                            {' '} eSHOP
+                        </Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
