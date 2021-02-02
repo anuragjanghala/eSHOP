07a9027b6565d5f3f5f85a57452cd6f8f46958a6
anuragjanghala <ajanghala@gmail.com>
2021-02-02 23:26:49 +0000

    added functionality for mongodb
    installed mongoose for working with js modules to use mongodb and created setup for that in config folder in db.js file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import products from './data/products.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
+
+connectDB()
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
