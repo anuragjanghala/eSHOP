573dcd5478b5f99d2a56940c3af4575b3113210a
anuragjanghala <ajanghala@gmail.com>
2021-02-07 00:47:12 +0000

    added action, reducer and constant for cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,22 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailReducer } from './reducers/productReducers.js'
+import { cartReducer } from './reducers/cartReducers.js'
+
+
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailReducer,
-})
+    cart: cartReducer,
+}) 
 
-const initialState = {}
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItem')) : []
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage},
+}
 
 const middleware = [thunk]
 
