ae05754a92ca47fc8af8de10089373f5d9ef1d03
anuragjanghala <ajanghala@gmail.com>
2021-02-11 21:20:04 +0000

    added usercontroller and userroutes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -13,11 +14,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running..')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 
 app.use(notFound)
