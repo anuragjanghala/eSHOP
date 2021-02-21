22bb06a593f4291324edd5eb4be5939c90323475
anuragjanghala <ajanghala@gmail.com>
2021-02-21 23:54:37 +0000

    added place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from './screens/RegisterScreen.js'
 import ProfileScreen from './screens/ProfileScreen.js'
 import ShippingScreen from './screens/ShippingScreen.js'
 import PaymentScreen from './screens/PaymentScreen.js'
+import PlaceOrderScreen from './screens/PlaceOrderScreen.js'
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path='/profile' component={ProfileScreen}/>
         <Route path='/shipping' component={ShippingScreen}/>
         <Route path='/payment' component={PaymentScreen}/>
+        <Route path='/placeorder' component={PlaceOrderScreen}/>
       </Container>
       
     </main>
