a219f2fb7c6efed4ebe29ef459d770d937356a30
anuragjanghala <ajanghala@gmail.com>
2021-03-08 20:16:27 +0000

    backend admin-conroller-middleware and getUsers route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,20 @@
-import express from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import express from "express";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+} from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/').post(registerUser)
-router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route("/").post(registerUser).get(protect, admin, getUsers);
+router.post("/login", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
-export default router
+export default router;
