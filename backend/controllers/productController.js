bd7d92bfc9420ab685fdd9113eb6d36bc200d8cc
anuragjanghala <ajanghala@gmail.com>
2021-03-11 18:58:31 +0000

    test producteditscreen

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -46,7 +46,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: "Sample Name",
     price: 0,
     user: req.user._id,
-    image: "/images/sample.jpg",
+    image: "/images/samples.jpg",
     brand: "Sample Brand",
     category: "Sample Category",
     countInStock: 0,
